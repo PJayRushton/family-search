9ca698a9150ea5ac6a8b396fbd9a9c213f1b59e3
Parker Rushton <parkerrushton@gmail.com>
2026-09-15 09:05:00 -0600

    refactor: separate relative entity

--- FamilySearch/Data/Persistence/PersonEntity.swift
+++ FamilySearch/Data/Persistence/PersonEntity.swift
@@ -50,26 +50,3 @@ final class PersonEntity {
         self.relatives = relatives
     }
 }
-
-@Model
-final class RelativeEntity {
-    var personID: String
-    var relationship: String
-    var givenName: String
-    var surname: String
-    var birthYear: Int
-    var deathYear: Int?
-    var person: PersonEntity?
-
-    init(
-        personID: String, relationship: String, givenName: String, surname: String,
-        birthYear: Int, deathYear: Int?
-    ) {
-        self.personID = personID
-        self.relationship = relationship
-        self.givenName = givenName
-        self.surname = surname
-        self.birthYear = birthYear
-        self.deathYear = deathYear
-    }
-}
